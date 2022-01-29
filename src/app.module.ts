559e9c2d69c4b2e3f57caeeeaf4587acd9f75e90
Mikhail <misharygin@gmail.com>
2022-01-29 20:05:05 +0300

    innitial

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule, 
+    MongooseModule.forRoot(`mongodb+srv://mikhail:mik1234@cluster0.rnept.mongodb.net/products?retryWrites=true&w=majority`)
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
